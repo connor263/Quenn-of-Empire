4f1e5e69ab58a58d5a99023c3a1a6ae962658acd
c0nnor263 <bojchu7@gmail.com>
2022-06-09 11:15:49 +0300

    Update

--- app/src/main/java/com/samsung/android/sclou/ui/InitScreen.kt
+++ app/src/main/java/com/samsung/android/sclou/ui/InitScreen.kt
@@ -1,11 +1,10 @@
 package com.samsung.android.sclou.ui
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -18,30 +17,35 @@ fun InitScreen() {
     var isLoading by remember { mutableStateOf(true) }
 
     Surface(Modifier.fillMaxSize(), color = Color.Black) {
-        if (isLoading) {
-            CircularProgressIndicator(Modifier.fillMaxSize().size(120.dp), color = Color.White)
-        } else {
-            AlertDialog(
-                modifier = Modifier.padding(4.dp),
-                shape = RoundedCornerShape(16.dp),
-                title = {
-                    Text(
-                        "No internet connection",
-                        style = MaterialTheme.typography.h4
-                    )
-                },
-                text = {
-                    Text(
-                        "Check your internet connection and try again later",
-                        style = MaterialTheme.typography.body2
-                    )
-                },
-                onDismissRequest = { viewModel.sendIifwesbxwxuzmyspEvent() },
-                confirmButton = {
-                    TextButton(onClick = { viewModel.sendIifwesbxwxuzmyspEvent() }) {
-                        Text("Try again", style = MaterialTheme.typography.button)
-                    }
-                })
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            if (isLoading) {
+                CircularProgressIndicator(Modifier.size(120.dp), color = Color.White)
+            } else {
+                AlertDialog(
+                    modifier = Modifier.padding(4.dp),
+                    shape = RoundedCornerShape(16.dp),
+                    title = {
+                        Text(
+                            "No internet connection",
+                            style = MaterialTheme.typography.h4
+                        )
+                    },
+                    text = {
+                        Text(
+                            "Check your internet connection and try again later",
+                            style = MaterialTheme.typography.body2
+                        )
+                    },
+                    onDismissRequest = { viewModel.sendIifwesbxwxuzmyspEvent() },
+                    confirmButton = {
+                        TextButton(onClick = { viewModel.sendIifwesbxwxuzmyspEvent() }) {
+                            Text("Try again", style = MaterialTheme.typography.button)
+                        }
+                    })
+            }
         }
     }
 
